090019c01dc040c8715bb7ab3645401e7fe1b682
DumbDrumb <basirov.ernest@yandex.ru>
2016-11-16 17:56:50 +0300

    NHJBVHJGV

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.*;
+public class Main {
+    public static Scanner in=new Scanner(System.in);
+    public static void main(String[] args) {
+        Frac a1 = new Frac(22,11);
+        Frac a2 = new Frac(1);
+        System.out.println(a1);
+
+IOJHKJGHKJH
+    }
+
+    }
